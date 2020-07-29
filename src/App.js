8201219eee24840b24c0d458df6d2b6db78ed1b8
rbooko <ramen.booko@bell.ca>
2020-07-28 22:37:48 -0400

    first feature

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ramenator <code>src/App.js</code>.
+          Ramenator is best.
         </p>
         <a
           className="App-link"
